1c3d089ba3b16a38e4771154e6e6d919a6c17979
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-11-10 10:24:43 +0100

    Add project files.

---
+++ CA201110/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA201110
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sw = new StreamWriter(@"proba.txt");
+            sw.WriteLine("Hello World!\nZoli vagyok!\nSzeretem a csincsillákat!");
+            Console.WriteLine("kész");
+            sw.Close();
+            Console.ReadKey();
+        }
+    }
+}
